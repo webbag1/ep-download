240de63bb2327af8d438f6a37cc8f261fb96f9ac
webbag <139613375+webbag1@users.noreply.github.com>
2025-01-17 12:24:09 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname)));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 
 async function getEpisodeLink(episode) {
