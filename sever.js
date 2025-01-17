c217f92b4e98c81e9b4c03509cf231508d6e705a
webbag <139613375+webbag1@users.noreply.github.com>
2025-01-17 12:46:51 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
