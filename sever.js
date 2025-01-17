762a492e481c5c6ec7051334eeaa4d210c25d9ff
webbag <139613375+webbag1@users.noreply.github.com>
2025-01-17 14:17:28 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -6,7 +6,7 @@ const cheerio = require("cheerio");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
